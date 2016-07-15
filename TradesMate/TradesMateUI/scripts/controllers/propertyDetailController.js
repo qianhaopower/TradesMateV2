aa3628adc7975bc3e6fae0d7a573a7f52f6b4183
Hao Qian <hao.qian@mainpac.com.au>
2016-07-15 13:54:15 +1000

    add property pages

---
+++ TradesMate/TradesMateUI/scripts/controllers/propertyDetailController.js
@@ -0,0 +1,82 @@
+'use strict';
+
+angular.module('sbAdminApp')
+  .controller('propertyDetailController', ['$scope', 'propertyDataService', 'clientDataService',
+      'Notification', '$state', '$stateParams',
+function ($scope, propertyDataService, clientDataService, Notification, $state, $stateParams) {
+
+    $scope.filterTextModel = {
+        searchText: undefined,
+    };
+    $scope.propertyId = undefined;
+    $scope.property = undefined;
+    $scope.clientId = undefined;
+    $scope.client = undefined;
+
+    $scope.readOnly = $state.current.name == 'dashboard.viewProperty';
+
+    
+
+    $scope.save = function () {
+        $scope.property.address = undefined;
+
+        if ($scope.property.isNew) {
+            var propertyCopy = angular.copy($scope.property);
+            propertyCopy.clientId = $stateParams.clientId;
+            propertyDataService.createProperty(propertyCopy).then(function (result) {
+                Notification.success({ message: 'Created', delay: 2000 });
+                $scope.goToPropertyIndex();
+            }, function (error) { Notification.error({ message: error, delay: 2000 }); });
+        } else {
+            propertyDataService.editProperty($scope.property).then(function (result) {
+                Notification.success({ message: 'Saved', delay: 2000 });
+                $scope.goToPropertyIndex();
+            }, function (error) { Notification.error({ message: error, delay: 2000 }); });
+        }
+    
+    }
+    $scope.goToPropertyIndex = function () {
+        if ($scope.property.clientId)
+            $state.go('dashboard.clientProperties', { param: $scope.property.clientId });
+        else
+            $state.go('dashboard.properties');
+    };
+
+
+    var init = function () {
+
+        if ($state.current.name == 'dashboard.createProperty') {
+            $scope.clientId = $stateParams.clientId;
+        } else {
+            $scope.propertyId = $stateParams.propertyId;
+        }
+       
+        if ($state.current.name == 'dashboard.editProperty' || $state.current.name == 'dashboard.viewProperty') {
+            propertyDataService.getPropertyById($scope.propertyId).then(function (result) {
+                $scope.property = result;
+                $scope.clientId = result.clientId;
+
+                if ($scope.clientId) {
+                    clientDataService.getClientById($scope.clientId).then(function (result) {
+                        $scope.client = result
+
+                    }, function (error) { Notification.error({ message: error, delay: 2000 }); });
+                }
+
+            }, function (error) { Notification.error({ message: error, delay: 2000 }); });
+        } else if ($state.current.name == 'dashboard.createProperty') {
+            //create new property
+            $scope.property = {
+                condition:'Normal',//default
+                isNew: true,
+            }
+        }
+        //propertyDataService.getAllPropertys().then(function (result) {
+        //    $scope.propertylist = result;
+        //}, function (error) { Notification.error({ message: error, delay: 2000 }); });
+    }
+
+
+    init();
+
+}]);
