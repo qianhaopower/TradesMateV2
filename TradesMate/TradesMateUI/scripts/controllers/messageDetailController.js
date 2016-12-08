86b71aa627c5902dc0cd92ab6298d71e73c714d5
Hao Qian <hao.qian@mainpac.com.au>
2016-12-08 16:10:42 +1100

    Move messageDetail to directive

--- TradesMate/TradesMateUI/scripts/controllers/messageDetailController.js
+++ TradesMate/TradesMateUI/scripts/controllers/messageDetailController.js
@@ -1,94 +1,17 @@
 'use strict';
 
 angular.module('sbAdminApp')
-  .controller('messageDetailController', ['$scope', 'messageService', 'Notification', '$state', '$stateParams', '$sce', 'ModalService',
-function ($scope, messageService, Notification, $state, $stateParams, $sce, modalService) {
+  .controller('messageDetailController', ['$scope',  '$stateParams',
+function ($scope,$stateParams) {
 
    
-    $scope.message = {};
+   
     $scope.goBack = function () {
         $state.go('base.message');
     };
 
+    $scope.messageId = $stateParams.messageId;
     
-
-    $scope.messageTitle = undefined;
-    $scope.showRespond = false;
-
-    //Accept = 0,
-    //Reject = 1,
-    $scope.accept = function () {
-        handle(0);
-    }
-
-    $scope.reject = function () {
-        handle(1);
-    }
-    $scope.status = {
-        isRespondOpen: true,
-        isMessageOpen: true,
-
-    };
-    $scope.renderHtml = function (html_code) {
-        return $sce.trustAsHtml(html_code);
-    };
-
-  
-
-
-    var handle = function (action) {
-        messageService.handleMessageResponse($scope.message.id, action).then(function (result) {
-            Notification.success({ message: action ? 'Message rejected' : 'Message accepted', delay: 2000 });
-            init();
-        }, function (error) { Notification.error({ message: error, delay: 2000 }); });
-    }
-
-
-    var init = function () {
-        messageService.getMessageById($stateParams.messageId).then(function (result) {
-
-            $scope.message = result;
-            $scope.showRespond = result.messageResponse;
-            $scope.respondText = result.messageResponse
-                ? result.memberName + ' has ' + result.messageResponse.responseAction.toLocaleLowerCase() + ' this request.'
-                : undefined;
-            $scope.messageTitle = messageService.getMessageTitleForType($scope.message.messageType);
-            //if(!result.isRead)//mark as read
-
-            //server will handle should we proceed this.
-            messageService.markMessageAsRead($scope.message.id);
-
-            //if (result.messageResponse && !result.messageResponse.isRead)//mark response as read, we always mark it, server handles if this is the right person reading it and decide if we mark it.
-            //    messageService.markResponseAsRead($scope.message.id);//message and respond share the same key.
-
-
-        }, function (error) { Notification.error({ message: error, delay: 2000 }); });
-    }
-
-    $scope.createProperty = function () {
-        modalService.showModal({
-            templateUrl: 'createPropertyFromRequest.html',
-            controller: "createPropertyFromRequestController",
-            inputs: {
-                addressFormatted: $scope.message.propertyAddress,
-
-            }
-        }).then(function (modal) {
-            modal.element.modal();
-            modal.close.then(function (property) {
-                if (property) {
-                    //property should be created, reload this message detail
-
-                    messageService.createPropertyForWorkRequest($stateParams.messageId, property).then(function (result) {
-                        init();
-                    }, function (error) { Notification.error({ message: error, delay: 2000 }); });
-                  
-                }
-            });
-        });
-    }
-
-    init();
 
 }]);
 
@@ -96,20 +19,3 @@ function ($scope, messageService, Notification, $state, $stateParams, $sce, moda
 
 
 
-angular.module('sbAdminApp').controller('createPropertyFromRequestController', function ($scope, addressFormatted, close) {
-    $scope.property = {
-        name: undefined,
-        description: undefined,
-        address: {
-            line1: undefined,
-        },
-    };
-    $scope.addressFormatted = addressFormatted;
-    $scope.cancel = function () {
-        close(undefined, 500); // close, but give 500ms for bootstrap to animate
-    };
-    $scope.save = function () {
-        //fire the save
-        close($scope.property, 500);
-    }
-});
