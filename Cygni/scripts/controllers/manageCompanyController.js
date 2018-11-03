e2f88b8ca59d1760954165ba9fd6186139f789ee
hao-qian <hao.qian@xero.com>
2018-11-03 18:06:23 +1100

    Allow upload csv for client

--- Cygni/scripts/controllers/manageCompanyController.js
+++ Cygni/scripts/controllers/manageCompanyController.js
@@ -94,6 +94,40 @@ angular.module('sbAdminApp').controller('manageCompanyController', ['$scope', '$
         },function (error) { Notification.error({ message: error, delay: 2000 }); });
     }
     getCompanyDetail();
+
+
+    $scope.$watch('fileContent', function() {
+        if($scope.fileContent){
+          var clientJson=   csvJSON($scope.fileContent)
+        alert(clientJson);
+
+        }
+    });
+
+    function csvJSON(csv){
+
+        var lines=csv.split("\n");
+      
+        var result = [];
+      
+        var headers=lines[0].split(",");
+      
+        for(var i=1;i<lines.length;i++){
+      
+            var obj = {};
+            var currentline=lines[i].split(",");
+      
+            for(var j=0;j<headers.length;j++){
+                obj[headers[j]] = currentline[j];
+            }
+      
+            result.push(obj);
+      
+        }
+        
+        //return result; //JavaScript object
+        return JSON.stringify(result); //JSON
+      }
     }
 
 ]);
